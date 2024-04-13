926bd2019a1cf68831723ed656faf37092e82f6a
1101-22 <alya.korobeynikova99@gmail.com>
2024-04-13 13:30:16 +0300

    0.5

--- app/src/main/java/com/korobeynikova/pr11_zagadks/MainActivity.kt
+++ app/src/main/java/com/korobeynikova/pr11_zagadks/MainActivity.kt
@@ -11,88 +11,89 @@ import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 
-private lateinit var textRiddle: TextView
-private lateinit var radioGroup: RadioGroup
-private lateinit var btnCheck: Button
-private lateinit var btnStats: Button
-private lateinit var textRiddleCount: TextView
-
-private val answers = listOf(
-    Triple(
-        "Висит груша, нельзя скушать.",
-        listOf("Груша", "Шар", "Лампочка", "Яблоко"),
-        2
-    ),
-    Triple(
-        "Что можно сломать, но нельзя увидеть?",
-        listOf("Сердце", "Шар", "Воздух", "Доверие"),
-        0
-    ),
-    Triple(
-        "Что можно увидеть с закрытыми глазами?",
-        listOf("Звезды", "Сны", "Цветы", "Тени"),
-        1
-    ),
-    Triple("Кто его делает, тот не пользуется, кто пользуется, тот его не делает. Что это?", listOf("Ключ", "Гроб", "Ложка", "Ведро"), 1),
-    Triple(
-        "Что можно увидеть вместе с его создателем?",
-        listOf("Душа", "Тень", "Свет", "Отражение"),
-        1
-    ),
-    Triple(
-        "Что идет, не шагает, что идет, не едет?",
-        listOf("Дождь", "Свет", "Время", "Путь"),
-        2
-    ),
-    Triple(
-        "День спит, ночь глядит, утром умирает, другой сменяет. Что это?",
-        listOf("Месяц", "Свечи", "Вампир", "Ветер"),
-        1
-    ),
-    Triple("Хоть без глаз, могу бегущих догонять, но только никому меня нельзя обнять. Что это?", listOf("Туча", "Тень", "Деньги", "Звезда"), 1),
-    Triple(
-        "Без языка, а сказывается. Что это?",
-        listOf("Боль", "Жест", "Лай", "Старость"),
-        0
-    ),
-    Triple(
-        "Не море, не земля, корабли не плавают, и ходить нельзя. Что за место такое?",
-        listOf("Небеса", "Лава", "Болото", "Пустыня"),
-        2
-    ),
-    Triple(
-        "Первый говорит — побежим, другой говорит — полежим, третий говорит — покачаемся. Кто первый?",
-        listOf("Время", "Грунтовая дорога", "Конь", "Вода"),
-        3
-    ),
-    Triple(
-        "Деревянные ноги, хоть всё лето стой. Что это за зверь такой?",
-        listOf("Забор", "Ткацкий станок", "Табуретка", "Протезы"),
-        1
-    ),
-    Triple(
-        "В лесу выросло, из лесу вынесли, на руках плачет, а по полу скачут. Что это?",
-        listOf("Лапти", "Балалайка", "Деревянные доски", "Лук и стрелы"),
-        1
-    ),
-    Triple(
-        "Четыре четырки, две растопырки, седьмой вертун, два стёклушка в нём. Что это?",
-        listOf("Бык", "Часы", "Автомобиль", "Бинокль"),
-        0
-    ),
-    Triple(
-        "Утка в море, хвост на заборе. Что за чудо-юдо?",
-        listOf("Ковш", "Морковь", "Буй", "Якорь"),
-        0
-    )
-)
-
-private var currentRiddles: List<Triple<String, List<String>, Int>> = emptyList()
-private var currentRiddleIndex = 0
-private var correctAnswersCount = 0
-private var incorrectAnswersCount = 0
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textRiddle: TextView
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var btnCheck: Button
+    private lateinit var btnStats: Button
+    private lateinit var btnRiddle: Button
+    private lateinit var textRiddleCount: TextView
+
+    private val answers = listOf(
+        Triple(
+            "Висит груша, нельзя скушать.",
+            listOf("Груша", "Шар", "Лампочка", "Яблоко"),
+            2
+        ),
+        Triple(
+            "Что можно сломать, но нельзя увидеть?",
+            listOf("Сердце", "Шар", "Воздух", "Доверие"),
+            0
+        ),
+        Triple(
+            "Что можно увидеть с закрытыми глазами?",
+            listOf("Звезды", "Сны", "Цветы", "Тени"),
+            1
+        ),
+        Triple("Кто его делает, тот не пользуется, кто пользуется, тот его не делает. Что это?", listOf("Ключ", "Гроб", "Ложка", "Ведро"), 1),
+        Triple(
+            "Что можно увидеть вместе с его создателем?",
+            listOf("Душа", "Тень", "Свет", "Отражение"),
+            1
+        ),
+        Triple(
+            "Что идет, не шагает, что идет, не едет?",
+            listOf("Дождь", "Свет", "Время", "Путь"),
+            2
+        ),
+        Triple(
+            "День спит, ночь глядит, утром умирает, другой сменяет. Что это?",
+            listOf("Месяц", "Свечи", "Вампир", "Ветер"),
+            1
+        ),
+        Triple("Хоть без глаз, могу бегущих догонять, но только никому меня нельзя обнять. Что это?", listOf("Туча", "Тень", "Деньги", "Звезда"), 1),
+        Triple(
+            "Без языка, а сказывается. Что это?",
+            listOf("Боль", "Жест", "Лай", "Старость"),
+            0
+        ),
+        Triple(
+            "Не море, не земля, корабли не плавают, и ходить нельзя. Что за место такое?",
+            listOf("Небеса", "Лава", "Болото", "Пустыня"),
+            2
+        ),
+        Triple(
+            "Первый говорит — побежим, другой говорит — полежим, третий говорит — покачаемся. Кто первый?",
+            listOf("Время", "Грунтовая дорога", "Конь", "Вода"),
+            3
+        ),
+        Triple(
+            "Деревянные ноги, хоть всё лето стой. Что это за зверь такой?",
+            listOf("Забор", "Ткацкий станок", "Табуретка", "Протезы"),
+            1
+        ),
+        Triple(
+            "В лесу выросло, из лесу вынесли, на руках плачет, а по полу скачут. Что это?",
+            listOf("Лапти", "Балалайка", "Деревянные доски", "Лук и стрелы"),
+            1
+        ),
+        Triple(
+            "Четыре четырки, две растопырки, седьмой вертун, два стёклушка в нём. Что это?",
+            listOf("Бык", "Часы", "Автомобиль", "Бинокль"),
+            0
+        ),
+        Triple(
+            "Утка в море, хвост на заборе. Что за чудо-юдо?",
+            listOf("Ковш", "Морковь", "Буй", "Якорь"),
+            0
+        )
+    )
+
+    private var currentRiddles: List<Triple<String, List<String>, Int>> = emptyList()
+    private var currentRiddleIndex = 0
+    private var correctAnswersCount = 0
+    private var incorrectAnswersCount = 0
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -103,8 +104,9 @@ class MainActivity : AppCompatActivity() {
         btnCheck = findViewById(R.id.btn_check)
         btnStats = findViewById(R.id.btn_stats)
         textRiddleCount = findViewById(R.id.text_riddle_count)
+        btnRiddle = findViewById(R.id.btn_riddle)
 
-        btnCheck.isActivated = false
+        btnCheck.visibility = View.GONE
 
         btnCheck.setOnClickListener {
             checkAnswer()
@@ -114,7 +116,6 @@ class MainActivity : AppCompatActivity() {
             showStats()
         }
 
-        val btnRiddle = findViewById<Button>(R.id.btn_riddle)
         btnRiddle.setOnClickListener {
             showNextRiddle()
         }
@@ -179,8 +180,8 @@ class MainActivity : AppCompatActivity() {
             btnCheck.visibility = View.VISIBLE
         } else {
             btnCheck.visibility = View.GONE
-            btnStats.isEnabled = true
-            btnStats.alpha = 1.0f
+            btnStats.visibility = View.VISIBLE
+            btnRiddle.visibility = View.GONE
             Toast.makeText(this, "Все загадки отгаданы", Toast.LENGTH_SHORT).show()
         }
     }
